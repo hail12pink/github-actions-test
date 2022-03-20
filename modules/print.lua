f95f37577a72be4dd0d91defbd941ec24b1a6f54
hail12pink <59028187+hail12pink@users.noreply.github.com>
2022-03-20 18:40:52 -0400

    fixed syntax error! wowzers what a dummie did this!

--- modules/print.lua
+++ modules/print.lua
@@ -8,6 +8,6 @@ module.randomError = function()
   if math.random(0,1) == 1 then
     error("who could have expected that", 2)
   else
-    print("who could have expected that)
+    print("who could have expected that")
   end
 end
