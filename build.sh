70c2c0142782ae1c46baf28a82325a879347f2c4
hail12pink <hail12pink@gmail.com>
2022-03-20 17:39:33 -0400

    sus

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 chmod +x build.lua
-> build.lua #clear build.lua
+> build.lua #we clear build.lua
 
 for FILE in modules/*; do
 	echo $FILE;
 	chmod +x $FILE
-	CONTENTS=`cat $FILE`
-	echo $CONTENTS >> build.lua -- imported from ../$FILE
+	cat $FILE >> build.lua -- imported from ../$FILE
 done
 
 
-echo `cat main.lua` >> build.lua
+cat main.lua` >> build.lua
 
 git init
 git config --global user.name "robot"
