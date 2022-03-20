f9177230ae36b6eeff55da2b34fa120434598372
hail12pink <59028187+hail12pink@users.noreply.github.com>
2022-03-20 18:35:50 -0400

    Update print.lua

--- modules/print.lua
+++ modules/print.lua
@@ -3,3 +3,11 @@ local module = {}
 module.print = function()
   print("success!")
 end
+
+module.randomError = function()
+  if math.random(0,1) == 1 then
+    error("who could have expected that", 2)
+  else
+    print("who could have expected that)
+  end
+end
