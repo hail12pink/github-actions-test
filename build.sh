22472194c866e342a5d0b4bf1f20c1690a3ffee0
hail12pink <59028187+hail12pink@users.noreply.github.com>
2022-03-20 17:49:21 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 chmod +x build.lua
-> build.lua #we clear build.lua
+> build.lua # we clear build.lua
 
 for FILE in modules/*; do
 	echo $FILE;
 	chmod +x $FILE
-	echo "$(<FILE)" >> build.lua -- imported from ../$FILE
+	echo "$(<$FILE)" >> build.lua -- imported from ../$FILE # insert the module code into the build.lua file
 done
 
-echo "$(<main.lua)" >> build.lua
+echo "$(<main.lua)" >> build.lua # insert the main code into the build.lua file
 
 git init
 git config --global user.name "robot"
